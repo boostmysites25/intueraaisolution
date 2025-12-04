0a82d8f53fc160fde925898fe5b919f78b0563f9
Pranav <mpranavprem@gmail.com>
2025-12-04 14:52:00 +0530

    changes

--- src/util/constant.js
+++ src/util/constant.js
@@ -53,8 +53,7 @@ import { FaXTwitter } from "react-icons/fa6";
 
 export const companyDetails = {
   name: "IntueraAi Solution",
-  email: "mpranavprem@gmail.com",
-  // email: "intueraaisolution@gmail.com",
+  email: "intueraaisolution@gmail.com",
   phone: "+91-9510880965",
   address: "India",
   socialLinks: [
